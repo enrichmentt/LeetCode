f521fddbfddf266efe60d8a2618b73e70873e5ad
enrichmentt <enrichment.off@gmail.com>
2023-10-15 11:45:22 +0300

    P13

---
+++ Easy/P13.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    // https://leetcode.com/problems/roman-to-integer/
+    public class P13
+    {
+        private static readonly Dictionary<char, int> romanNumbers = new Dictionary<char, int>()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static int RomanToInt(string s)
+        {
+            var result = 0;
+            var newValue = 0;
+            var oldValue = 0;
+
+            foreach (var c in s)
+            {
+                newValue = romanNumbers[c];
+
+                if (newValue > oldValue)
+                    result = result + newValue - 2 * oldValue;
+                else
+                    result = result + newValue;
+
+                oldValue = newValue;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/P13_Test.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Easy;
+
+namespace LeetCode.Tests
+{
+    public class P13_Test
+    {
+        [Test]
+        public void Test1()
+        {
+            var input = "III";
+            int output = 3;
+
+            var result = P13.RomanToInt(input);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var input = "LVIII";
+            int output = 58;
+
+            var result = P13.RomanToInt(input);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var input = "MCMXCIV";
+            int output = 1994;
+
+            var result = P13.RomanToInt(input);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var input = "MMXXIII";
+            int output = 2023;
+
+            var result = P13.RomanToInt(input);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var input = "MMMMMDCCCLXIII";
+            int output = 5863;
+
+            var result = P13.RomanToInt(input);
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+    }
+}
