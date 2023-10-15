96d7a8863a6090e0cc485fa8e0db6210809b3192
enrichmentt <enrichment.off@gmail.com>
2023-10-15 11:47:49 +0300

    P1

---
+++ Easy/P1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    public class P1
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] != target) continue;
+                    if (i == j)
+                        continue;
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/P1_Test.cs
@@ -0,0 +1,59 @@
+﻿using Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    internal class P1_Test
+    {
+        [Test]
+        public void TwoSum1()
+        {
+            int[] arr = { 2, 7, 11, 15 };
+            var target = 9;
+            var result = P1.TwoSum(arr, target);
+            var output = new int[] { 0, 1 };
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TwoSum2()
+        {
+            int[] arr = { 3, 2, 4 };
+            var target = 7;
+            var result = P1.TwoSum(arr, target);
+
+            var output = new int[] { 0, 2 };
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TwoSum3()
+        {
+            int[] arr = { -2, 2, 4, 1, 3 };
+            var target = 0;
+            var result = P1.TwoSum(arr, target);
+
+            var output = new int[] { 0, 1 };
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void TwoSum4()
+        {
+            int[] arr = { -2, -1, 4, 1, 3 };
+            var target = -3;
+            var result = P1.TwoSum(arr, target);
+
+            var output = new int[] { 0, 1 };
+
+            Assert.That(output, Is.EqualTo(result));
+        }
+    }
+}
