11226daab5372bf54fba197c8c244a1d832b8964
enrichmentt <enrichment.off@gamil.com>
2023-10-16 09:54:08 +0300

    P9

---
+++ Easy/P9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    public class P9
+    {
+        public static bool IsPalindrome(int x)
+        {
+            var intToString = x.ToString();
+            int i = 0;
+
+            do
+            {
+                if (intToString[i] != intToString[intToString.Length - i - 1])
+                    return false;
+
+                i++;
+
+            } while (i < intToString.Length / 2 );
+            
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Tests/P9_Test.cs
@@ -0,0 +1,57 @@
+﻿using Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class P9_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = P9.IsPalindrome(10);
+            var expected = false;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = P9.IsPalindrome(121);
+            var expected = true;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = P9.IsPalindrome(-121);
+            var expected = false;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = P9.IsPalindrome(1221);
+            var expected = true;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var result = P9.IsPalindrome(1000021);
+            var expected = false;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
