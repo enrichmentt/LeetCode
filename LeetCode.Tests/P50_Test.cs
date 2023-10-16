e32b61f0df99f749da2e8039ec072dd2c8da7029
enrichmentt <enrichment.off@gmail.com>
2023-10-16 22:55:32 +0300

    P50

---
+++ LeetCode.Tests/P50_Test.cs
@@ -0,0 +1,66 @@
+﻿using Medium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class P50_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = P50.MyPow(2.0, 2);
+
+            var expected = 4.0;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = P50.MyPow(2.0, -2);
+
+            var expected = 0.25;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = P50.MyPow(10.0, 0);
+
+            var expected = 1.0;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = P50.MyPow(-10, 2);
+
+            var expected = 100.0;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var result = P50.MyPow(1, -2147483648);
+
+            var expected = 1.0;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test6()
+        {
+            var result = P50.MyPow(0.00001, 2147483647);
+
+            var expected = 0.0;
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Medium/P50.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medium
+{
+    // https://leetcode.com/problems/powx-n/
+    public class P50
+    {
+        public static double MyPow(double x, int n)
+        {
+            if (n == 1)
+                return x;
+            if (n == 0)
+                return 1;
+            if (n == -1)
+                return 1 / x;
+            if (n == 2)
+                return x * x;
+            return MyPow(MyPow(x, n / 2), 2) * MyPow(x, n % 2);
+        }
+    }
+}
