79aef2cefc0175d8672ea7ff3f4c6231489b2a7d
enrichmentt <enrichment.off@gamil.com>
2023-10-17 10:08:38 +0300

    P217

---
+++ Easy/P217.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    // https://leetcode.com/problems/contains-duplicate/
+    public class P217
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for(int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ LeetCode.Tests/P217_Test.cs
@@ -0,0 +1,52 @@
+﻿using Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class P217_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = P217.ContainsDuplicate(new int[] { 1, 2, 3, 4, 5});
+
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = P217.ContainsDuplicate(new int[] { 1, 2, 3, 4, 5, 1 });
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = P217.ContainsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 });
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = P217.ContainsDuplicate(new int[] { 1, 2, 3, 4, 5, 1, 3 });
+
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var result = P217.ContainsDuplicate(new int[] { -5, 12, 144, -7, 0 , 2, 5 });
+
+            Assert.IsFalse(result);
+        }
+    }
+}
