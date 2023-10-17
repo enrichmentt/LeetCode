73870664e642d2c8aba3741df354d4d6d54dd428
enrichmentt <enrichment.off@gamil.com>
2023-10-17 16:19:11 +0300

    P283

---
+++ Easy/P283.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    public class P283
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            for(int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    var tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/P283_Test.cs
@@ -0,0 +1,36 @@
+﻿using Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class P283_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var numbers = new int[] { 0, 1, 0, 3, 12 };
+
+            P283.MoveZeroes(numbers);
+
+            var expected = new int[] { 1, 3, 12, 0, 0 };
+
+            Assert.That(numbers, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var numbers = new int[] { 0 };
+
+            P283.MoveZeroes(numbers);
+
+            var expected = new int[] { 0 };
+
+            Assert.That(numbers, Is.EqualTo(expected));
+        }
+    }
+}
