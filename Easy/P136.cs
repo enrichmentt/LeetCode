882edf3d0e0ec1e3204fe92dd3a9a395764062f4
enrichmentt <enrichment.off@gamil.com>
2023-10-17 10:52:33 +0300

    P136

---
+++ Easy/P136.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy
+{
+    // https://leetcode.com/problems/single-number
+    public class P136
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            return nums.Aggregate((acc, x) => acc ^ x);
+        }
+    }
+}
---
+++ LeetCode.Tests/P136_Test.cs
@@ -0,0 +1,48 @@
+﻿using Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class P136_Test
+    {
+        [Test]
+        public void Test()
+        {
+            var result = P136.SingleNumber(new int[] { 2, 2, 1 });
+            var expected = 1;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var result = P136.SingleNumber(new int[] { 4, 1, 2, 1, 2 });
+            var expected = 4;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var result = P136.SingleNumber(new int[] { 1 });
+            var expected = 1;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var result = P136.SingleNumber(new int[] { 1,5,3,5,3,6, 1 });
+            var expected = 6;
+
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
