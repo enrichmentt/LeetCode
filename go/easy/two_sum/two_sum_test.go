699159424311006d529de038be5a6b45d38b7bdb
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-10 14:57:41 +0300

    two sum go

---
+++ go/easy/two_sum/two_sum_test.go
@@ -0,0 +1,38 @@
+﻿package two_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		name   string
+		nums   []int
+		target int
+		want   []int
+	}{
+		{
+			name:   "Пример 1: Базовый случай",
+			nums:   []int{2, 7, 11, 15},
+			target: 9,
+			want:   []int{0, 1},
+		},
+		{
+			name:   "Пример 2: Другой порядок",
+			nums:   []int{3, 2, 4},
+			target: 6,
+			want:   []int{1, 2},
+		},
+		// Добавьте больше тестовых случаев сюда
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := TwoSum(tt.nums, tt.target)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TwoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ go/go.mod
@@ -0,0 +1,3 @@
+module github.com/enrichmentt/LeetCode
+
+go 1.26.1
