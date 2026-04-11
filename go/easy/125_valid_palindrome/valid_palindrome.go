cfeb2640774c36b40911b89c8e585a841afb9d34
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-11 23:18:38 +0300

    125

---
+++ go/easy/125_valid_palindrome/valid_palindrome.go
@@ -0,0 +1,32 @@
+﻿package valid_palindrome
+
+import (
+	"strings"
+	"unicode"
+)
+
+func IsPalindrome(s string) bool {
+	var sb strings.Builder
+
+	for _, v := range s {
+		if unicode.IsLetter(v) || unicode.IsDigit(v) {
+			sb.WriteRune(v)
+		}
+	}
+
+	s = sb.String()
+	s = strings.ToUpper(s)
+
+	start, end := 0, len(s)-1
+
+	for start < end {
+		if s[start] != s[end] {
+			return false
+		}
+
+		start++
+		end--
+	}
+
+	return true
+}
---
+++ go/easy/125_valid_palindrome/valid_palindrome_test.go
@@ -0,0 +1,26 @@
+﻿package valid_palindrome
+
+import (
+	"testing"
+)
+
+func TestIsValidPalindrome(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  bool
+	}{
+		{"test1", "A man, a plan, a canal: Panama", true},
+		{"test2", "0P", false},
+		{"test3", "race a car", false},
+		{"test4", " ", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome(tt.input); got != tt.want {
+				t.Errorf("%s got %t, want %t", tt.name, got, tt.want)
+			}
+		})
+	}
+}
