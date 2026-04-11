73509ec5bc1ffd1797e42e0460ecbfed9bbb864c
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-11 23:55:06 +0300

    283. Move Zeroes

---
+++ go/easy/283_move_zeroes/move_zeroes.go
@@ -0,0 +1,14 @@
+﻿package move_zeroes
+
+func MoveZeroes(nums []int) {
+	k := 0
+
+	for i, v := range nums {
+		if v == 0 {
+			continue
+		} else {
+			nums[k], nums[i] = nums[i], nums[k]
+			k++
+		}
+	}
+}
---
+++ go/easy/283_move_zeroes/move_zeroes_test.go
@@ -0,0 +1,27 @@
+﻿package move_zeroes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestMoveZeroes(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{"Test1", []int{0, 1, 0, 3, 0, 12}, []int{1, 3, 12, 0, 0, 0}},
+		{"Test2", []int{0}, []int{0}},
+		{"Test3", []int{0, 0, 0, 1, 1, 1}, []int{1, 1, 1, 0, 0, 0}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			MoveZeroes(tt.input)
+			if !reflect.DeepEqual(tt.want, tt.input) {
+				t.Errorf("%s got %v, want %v", tt.name, tt.input, tt.want)
+			}
+		})
+	}
+}
