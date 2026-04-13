1b180940efee5e88d1f957fc240bbd20bbd6f9e6
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-13 19:54:00 +0300

    26. Remove Duplicates from Sorted Array

---
+++ go/easy/26_remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.go
@@ -0,0 +1,14 @@
+﻿package remove_duplicates_from_sorted_array
+
+func RemoveDuplicates(nums []int) int {
+	k := 1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[i-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
---
+++ go/easy/26_remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array_test.go
@@ -0,0 +1,27 @@
+﻿package remove_duplicates_from_sorted_array
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRemoveDuplicates(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  int
+	}{
+		{"Test1", []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, 5},
+		{"Test2", []int{1, 1, 2}, 2},
+		{"Test3", []int{1, 1}, 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := RemoveDuplicates(tt.input)
+			if !reflect.DeepEqual(tt.want, r) {
+				t.Errorf("%s got %v wand %v", tt.name, tt.want, r)
+			}
+		})
+	}
+}
