4c1630da323c6db11f688e4885a134a9b8c10d4b
Vladimir Pekshev <enrichment.off@gmail.com>
2026-04-14 20:23:35 +0300

    349. Intersection of Two Arrays

---
+++ go/easy/349_intersection_of_two_arrays/intersection_of_two_arrays.go
@@ -0,0 +1,29 @@
+﻿package intersection_of_two_arrays
+
+import "sort"
+
+func Intersection(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+
+	intersectionMap := make(map[int]bool)
+
+	for i, j := 0, 0; i < len(nums1) && j < len(nums2); {
+		if nums1[i] < nums2[j] {
+			i++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else {
+			intersectionMap[nums1[i]] = true
+			i++
+			j++
+		}
+	}
+
+	result := []int{}
+	for num := range intersectionMap {
+		result = append(result, num)
+	}
+
+	return result
+}
---
+++ go/easy/349_intersection_of_two_arrays/intersection_of_two_arrays_test.go
@@ -0,0 +1,27 @@
+﻿package intersection_of_two_arrays
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIntersection(t *testing.T) {
+	tests := []struct {
+		name string
+		arr1 []int
+		arr2 []int
+		want []int
+	}{
+		{"Test1", []int{1, 2, 2, 1}, []int{2, 2}, []int{2}},
+		{"Test2", []int{4, 9, 5}, []int{9, 4, 9, 8, 4}, []int{9, 4}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := Intersection(tt.arr1, tt.arr2)
+			if !reflect.DeepEqual(tt.want, r) {
+				t.Errorf("%s got %v, want %v", tt.name, r, tt.want)
+			}
+		})
+	}
+}
